7925c6fd45f61283f765548c901d3537a0481505
Felipe-Ma <f_martinez8@u.pacific.edu>
2023-11-30 16:42:53 -0800

    Config works
    Added all config values retrieval

--- functions.py
+++ functions.py
@@ -1,4 +1,94 @@
 from datetime import datetime
+import os
+import yaml
+
+
+class Config:
+    def __init__(self, current_directory=None, config_path=None, credential_path=None,
+                 spreadsheet=None, worksheets=None):
+        self.current_directory = current_directory
+        self.config_path = config_path
+        self.credential_path = credential_path
+        self.spreadsheet = spreadsheet
+        self.worksheets = worksheets
+
+    def set_current_directory(self, current_directory):
+        self.current_directory = current_directory
+
+    def set_config_path(self, config_path):
+        self.config_path = config_path
+
+    def set_credential_path(self, credential_path):
+        self.credential_path = credential_path
+
+    def set_spreadsheet(self, spreadsheet):
+        self.spreadsheet = spreadsheet
+
+    def set_worksheets(self, worksheets):
+        self.worksheets = worksheets
+
+    # Print all config values
+    def __call__(self):
+        print("Current Directory: " + str(self.current_directory))
+        print("Config Path: " + str(self.config_path))
+        print("Credential Path: " + str(self.credential_path))
+        print("Spreadsheet: " + str(self.spreadsheet))
+        print("Worksheet: " + str(self.worksheets))
+
+
+def get_current_directory():
+    current_directory = "Unknown"
+    try:
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+    except Exception as e:
+        print(str(e) + "Error getting current directory")
+    return current_directory
+
+
+def get_config_path(main_directory):
+    config_path = "Unknown"
+    try:
+        config_path = os.path.join(main_directory, 'config.yaml')
+    except Exception as e:
+        print(str(e) + "Error getting config path!")
+    return config_path
+
+
+# Get Path of Credentials File
+def get_credential_path(config_path):
+    credentials_path = "Unknown"
+    try:
+        with open(config_path, 'r') as config_file:
+            config = yaml.safe_load(config_file)
+            credential_name = config['credentialsName']
+            # Extract the directory from the config_path
+            config_directory = os.path.dirname(config_path)
+            # Join the directory path with the credential file name
+            credentials_path = os.path.join(config_directory, credential_name)
+    except Exception as e:
+        print(str(e) + "Error getting credentials path!")
+    return credentials_path
+
+
+def get_spreadsheet(config_path):
+    spreadsheet = "Unknown"
+    try:
+        with open(config_path, 'r') as config_file:
+            config = yaml.safe_load(config_file)
+            spreadsheet = config['spreadsheet']
+    except Exception as e:
+        print(str(e) + "Error getting spreadsheet name!")
+    return spreadsheet
+
+def get_worksheets(config_path):
+    worksheets = "Unknown"
+    try:
+        with open(config_path, 'r') as config_file:
+            config = yaml.safe_load(config_file)
+            worksheets = config['worksheets']
+    except Exception as e:
+        print(str(e) + "Error getting worksheet names!")
+    return worksheets
 
 def get_time():
     current_datetime = datetime.now()
--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import logging
 from functions import *
 
 #get_time()
-start_time = time.time()
+#start_time = time.time()
 # Log in terminal
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-#logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s %(message)s')
 
-logging.info("Authorizing Google Sheets API")
-gc = pygsheets.authorize(service_file="testing.json")
+
+
 
 
 #print(gc.spreadsheet_ids())
@@ -21,14 +20,13 @@ gc = pygsheets.authorize(service_file="testing.json")
 #logging.info("Opening all spreadsheets")
 #wks = gc.open_all()
 
-logging.info("Opening spreadsheet 1")
-spreadsheet = gc.open("Server Dashboard")
+
 #spreadsheet = wks[1]
 
 # logging.info("Opening worksheet 1")
-print(spreadsheet)
+#print(spreadsheet)
 
-logging.info("Return worksheets as csv")
+#logging.info("Return worksheets as csv")
 #spreadsheet.export(path=os.path.dirname(os.path.realpath(__file__)), filename="worksheets.csv")
 #print(spreadsheets.worksheets())
 #worksheet1 = spreadsheet[0]
@@ -53,4 +51,31 @@ logging.info("Return worksheets as csv")
 #wks = sh.worksheet_by_title("Yoda")
 #print(wks.updated())
 
-print("--- %s seconds ---" % (time.time() - start_time))
+def authorize():
+    logging.info("Authorizing Google Sheets API")
+    gc = pygsheets.authorize(service_file="testing.json")
+
+    return gc
+
+    #logging.info("Opening spreadsheet 1")
+    #spreadsheet = gc.open("Server Dashboard")
+
+
+def retrieve_config_values(config):
+    config()
+    logging.info("Retrieving config values")
+    config.set_current_directory(get_current_directory())
+    config.set_config_path(get_config_path(config.current_directory))
+    config.set_credential_path(get_credential_path(config.config_path))
+    config.set_spreadsheet(get_spreadsheet(config.config_path))
+    config.set_worksheets(get_worksheets(config.config_path))
+    config()
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    config = Config()
+    retrieve_config_values(config)
+    #gc = authorize()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
